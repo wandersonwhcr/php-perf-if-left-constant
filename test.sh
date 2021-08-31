1a11d64a2f0de7e17b2fe49d353ad2f709ad5542
Wanderson Camargo <wandersonwhcr@gmail.com>
2021-08-31 11:42:52 -0300

    Execute two steps

--- test.sh
+++ test.sh
@@ -2,14 +2,19 @@
 
 set -xe
 
+TOTAL=2
+
 L=""
 R=""
 
-for i in `seq 1`; do
+for i in `seq $TOTAL`; do
     echo "seq=$i"
     L="$L+"`docker-compose run --rm php if-l-constant.php`
-    R="$R+"`docker-compose run --rm php if-r-constant.php`"+"
+    R="$R+"`docker-compose run --rm php if-r-constant.php`
 done;
 
-echo "$L"
-echo "$R"
+L=`echo "$L" | sed 's/^+//'`
+R=`echo "$R" | sed 's/^+//'`
+
+echo "scale=10;($L)/$TOTAL" | bc
+echo "scale=10;($R)/$TOTAL" | bc
